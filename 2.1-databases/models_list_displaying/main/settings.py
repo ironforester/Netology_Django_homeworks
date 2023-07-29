8ff0dc3bb0fd979c2bd2c76ef0b79978d5ec63c1
Belyakov <sano.belyakov@mail.ru>
2023-07-29 13:03:29 +0300

    hw3 solution added

--- 2.1-databases/models_list_displaying/main/settings.py
+++ 2.1-databases/models_list_displaying/main/settings.py
@@ -57,6 +57,9 @@ DATABASES = {
         'NAME': 'netology_models_list',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'sanobel',
+
     }
 }
 
--- 2.1-databases/work_with_database/main/settings.py
+++ 2.1-databases/work_with_database/main/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phones',
+    'phones.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
