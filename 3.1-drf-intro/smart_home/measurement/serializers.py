efc1cbf67a32b6a2c66634e7ae2ce1ec1dbd273c
Belyakov <sano.belyakov@mail.ru>
2023-08-08 11:23:25 +0300

    hw5 solution added

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,28 @@
+from measurement.models import Measurement, Sensor
 from rest_framework import serializers
 
-# TODO: опишите необходимые сериализаторы
+class MeasurementSerialazer(serializers.ModelSerializer):
+
+    class Meta():
+        model = Measurement
+        fields = ['temperature', 'created_at', 'image']
+
+
+class SensorDetailSerialaizer(serializers.ModelSerializer):
+    measurements = MeasurementSerialazer(read_only=True, many=True)
+
+    class Meta():
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
+
+
+class MeasurementAddSerialazer(serializers.ModelSerializer):
+    class Meta():
+        model = Measurement
+        fields = ['sensor', 'temperature', 'created_at', 'image']
+
+
+class SensorSerialaizer(serializers.ModelSerializer):
+    class Meta():
+        model = Sensor
+        fields = ['id', 'name', 'description']
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,20 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from measurement.models import Measurement, Sensor
+from measurement.serializers import MeasurementAddSerialazer, SensorDetailSerialaizer, SensorSerialaizer
+
+
+class SensorListView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerialaizer
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerialaizer
+
+
+class MeasurementView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementAddSerialazer
