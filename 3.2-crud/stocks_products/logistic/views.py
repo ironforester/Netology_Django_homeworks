ec596d33f784d0a923516730cad5e878e725d051
Belyakov <sano.belyakov@mail.ru>
2023-09-26 17:41:18 +0300

    view file changed

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -8,6 +8,7 @@ import datetime
 
 from django.http import HttpResponse
 
+
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -15,12 +16,12 @@ class ProductViewSet(ModelViewSet):
     search_fields = ['title', 'description']
 
 
-
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = [SearchFilter]
     search_fields = ['products__title', 'products__description']
+
 
 @api_view(['GET'])
 def time_view(request):
