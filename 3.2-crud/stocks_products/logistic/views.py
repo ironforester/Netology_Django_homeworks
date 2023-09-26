502e3f3a7e1e55ea98a110969cf6d798ea174b47
Belyakov <sano.belyakov@mail.ru>
2023-09-26 17:36:15 +0300

    view file changed

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,6 +1,7 @@
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 import datetime
@@ -21,9 +22,8 @@ class StockViewSet(ModelViewSet):
     filter_backends = [SearchFilter]
     search_fields = ['products__title', 'products__description']
 
-
+@api_view(['GET'])
 def time_view(request):
-
     current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    return Response(msg)
